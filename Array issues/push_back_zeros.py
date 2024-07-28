af76ce32ca84156ca92e29d9d634527870b5d904
otabek <baqara163@gmail.com>
2024-07-28 23:52:32 +0500

    Bismillah

---
+++ Array issues/push_back_zeros.py
@@ -0,0 +1,34 @@
+"""
+Nollarni orqaga surish
+
+Berilgan sonlar ro'yxatidagi nollarni ro'yxat orqasiga o'tkazing, lekin boshqa elementlar ketma-ketligi buzilmasin.
+
+Imkon qadar kamroq amal bajaring.
+
+Qo'shimcha xotiradan foydalanmang - amallarni ro'yxat ustida bajaring.
+Misol 1:
+
+Kiritish: nums = [0,1,0,3,12]
+
+Natija: [1, 3, 12, 0, 0]
+
+Misol 1:
+
+Kiritish: nums = [0]
+
+Natija: [0]
+"""
+
+
+def moveZeroes(nums: list) -> list:
+    count = 0
+
+    for i, num in enumerate(nums):
+        if num == 0:
+            count += 1
+            continue
+        nums[i], nums[i - count] = nums[i - count], nums[i]
+    return nums
+
+
+a = moveZeroes([1, 3, 0, 12, 0, 0])
